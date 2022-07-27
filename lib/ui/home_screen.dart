10c388cdead6a40db958b424ff7d90f657402519
Saikhnaa <macintosh@Macintoshs-MacBook-Pro.local>
2022-07-27 09:18:25 +0800

    d

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -8,29 +8,31 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
   String _text = "Hello world";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("hello"),
-              Text("hello1"),
-              MyInput(title: "Full Name", keyboardType: TextInputType.number,),
-              MyInput(title: "Email"),
-              MyInput(title: "Phone Number"),
-              MyInput(title: "Gender"),
-              MyInput(title: "Location"),
-            ],
+        body: SafeArea(
+            child: Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("hello"),
+          Text("hello1"),
+          MyInput(
+            title: "Full Name",
+            keyboardType: TextInputType.number,
           ),
-        )
-        )
-    );
+          MyInput(title: "Email"),
+          MyInput(title: "Phone Number"),
+          MyInput(title: "Gender"),
+          MyInput(title: "Location"),
+        ],
+      ),
+    )));
   }
 }

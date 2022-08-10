01a1a521c72afde3871ea6ee288d0c153283eef0
Saikhnaa <macintosh@Macintoshs-MacBook-Pro.local>
2022-08-10 15:31:15 +0800

    df

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -58,6 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
             label: 'account',
           ),
         ],
+        selectedItemColor: Colors.black,
       ),
     );
   }
---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:test/utils/routes.dart';
+import 'package:test/utils/sp_manager.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      SpManager sharedPreference = SpManager();
+      await sharedPreference.init();
+      String accessToken = await sharedPreference.getAccessToken();
+      print("accessToken: $accessToken");
+
+      Get.offAllNamed(accessToken.isNotEmpty ? homeRoute : loginRoute);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
